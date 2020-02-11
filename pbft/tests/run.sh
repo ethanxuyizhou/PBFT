eb65f01bed45a518dc5cb0ee5dbf6f1781eee85c
ethanxuyizhou <xuyizhou98@gmail.com>
2020-02-11 15:40:15 -0800

    Reorganize files

--- pbft/tests/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-build_dir="~/Desktop/project/ocaml/_build"
-~/Desktop/project/ocaml/_build/default/pbft/src/server/server.exe -ports 5000 -ports 6000 -ports 7000 -ports 8000 -me 0 &
-~/Desktop/project/ocaml/_build/default/pbft/src/server/server.exe -ports 5000 -ports 6000 -ports 7000 -ports 8000 -me 1 &
-~/Desktop/project/ocaml/_build/default/pbft/src/server/server.exe -ports 5000 -ports 6000 -ports 7000 -ports 8000 -me 2 &
-~/Desktop/project/ocaml/_build/default/pbft/src/server/server.exe -ports 5000 -ports 6000 -ports 7000 -ports 8000 -me 3 &
