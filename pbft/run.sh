4c634b1746f66bfa264e4369751b6c13749b6e19
ethanxuyizhou <xuyizhou98@gmail.com>
2020-03-04 16:32:30 -0500

    Automate run.sh to select server ports randomly

--- pbft/run.sh
+++ pbft/run.sh
@@ -5,15 +5,16 @@ client_command="${PBFT_BUILD_DIR}/client/client.exe"
 server_command="${PBFT_BUILD_DIR}/server/server.exe"
 
 host_and_ports=""
-count=$#
-for port
+count=$1
+ports=($(shuf -i 4000-5000 -n $1))
+for port in ${ports[@]}
 do
   host_and_ports+="-host-and-port localhost:$port "
 done
 
 me=0
 
-while (( $me < $# ))
+while (( $me < $count ))
 do
   eval "${server_command} ${host_and_ports}-me ${me} &"
   let "me+=1"
